d0a11d92e02091da0809864fe79909d5fcb505d5
Álef Davi <alefdavialves@gmail.com>
2026-04-16 12:33:26 -0300

    Adicionando ordem, lógica as perguntas e barra de preogresso

--- js/script.js
+++ js/script.js
@@ -1,21 +1,56 @@
 import { questions } from "./questions.js";
 
-let perguntaText = document.getElementById('pergunta');
-let perguntaNumero = document.getElementById('perguntaNumero');
+let perguntaText = document.getElementById("pergunta");
+let perguntaNumero = document.getElementById("perguntaNumero");
+let progresso = document.getElementsByClassName("progress-bar");
 
 let r1 = document.querySelector('label[for="r1"]');
 let r2 = document.querySelector('label[for="r2"]');
 let r3 = document.querySelector('label[for="r3"]');
 
-let numeroAleatorio = Math.floor(Math.random() * questions.length);
+let btnAvancar = document.getElementById("avancar");
 
-const pergunta = questions[numeroAleatorio];
-const opcoes = pergunta.options;
+let numeroAtual = 0;
+let jaUsados = [];
 
-perguntaText.innerText = pergunta.textQuestion;
+function gerarNumeroAleatorio() {
+  if (jaUsados.length === questions.length) {
+    alert("Fim do quiz!");
+    jaUsados = [];
+    numeroAtual = 0;
+  }
 
-r1.innerText = opcoes[0].option_text;
-r2.innerText = opcoes[1].option_text;
-r3.innerText = opcoes[2].option_text;
+  let numero;
 
-perguntaNumero.innerText = `Pergunta ${numeroAleatorio + 1}`;
+  do {
+    numero = Math.floor(Math.random() * questions.length);
+  } while (jaUsados.includes(numero));
+
+  jaUsados.push(numero);
+  return numero;
+}
+
+function carregarPergunta() {
+  const numeroAleatorio = gerarNumeroAleatorio();
+  const pergunta = questions[numeroAleatorio];
+  const opcoes = pergunta.options;
+
+  perguntaText.innerText = pergunta.textQuestion;
+
+  r1.innerText = opcoes[0].option_text;
+  r2.innerText = opcoes[1].option_text;
+  r3.innerText = opcoes[2].option_text;
+
+  numeroAtual++;
+
+  perguntaNumero.innerText = `Pergunta ${numeroAtual}`;
+
+  const porcentagem = (numeroAtual / questions.length) * 100;
+  progresso[0].style.width = porcentagem + "%";
+}
+
+carregarPergunta();
+
+btnAvancar.addEventListener("click", () => {
+  carregarPergunta();
+});
